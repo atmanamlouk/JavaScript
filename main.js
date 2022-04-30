3eab87885200f06f34536ccfa544a1095c90e588
atmanamlouk <atman.amlouk@gmail.com>
2022-04-30 02:21:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+//.first project on javaScript language
+/* first project*/ 
+console.log("Hello from JS")
+document.getElementById('test');
+let a="we"
+let b="love"
+let c="java"
+let d="script"
+console.log(a +" "+b +" "+c +" "+ d)
+console.log(a+" "+b+"\n"+c+" "+d)
+console.log(`${a} ${c}
+${c} ${d}`)
+console.log(`${a} ${c} ${c} ${d}`)
+
+let e="10";
+let f=20;
+let j=true;
+
+console.log(+e + f + j) 
+console.log(+e+f)
+console.log(Number(e))
+
+let h=10;
+let s="20";
+let m=80;
+
+console.log(++h + +s++ + +m++ - +h++);
+console.log(++h + -s+ + +m++ - -h++ + +h);
+console.log(--m + +s + --h * +s++ - +s * h + --h - +true);
